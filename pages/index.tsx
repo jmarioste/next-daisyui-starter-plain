98c67df2e0af343af08081d2d6498fc3012d10f3
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-01-11 17:27:15 +0800

    Update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const HomePage: NextPage = () => {
@@ -10,23 +9,6 @@ const HomePage: NextPage = () => {
       <div className="grid place-content-center min-h-screen">
         <div className="flex flex-col items-center gap-4">
           <h1 className="text-4xl my-8">Welcome to NextJS DaisyUI Starter</h1>
-          <Link className="btn " href="/admin">
-            Go to Admin
-          </Link>
-
-          {!!data?.user ? (
-            <>
-              <h2>You are logged in as {data?.user?.email} </h2>
-
-              <button className="btn" onClick={() => signOut()}>
-                Sign Out
-              </button>
-            </>
-          ) : (
-            <Link className="btn btn-outline" href="/signin">
-              Sign In
-            </Link>
-          )}
         </div>
       </div>
     </div>
